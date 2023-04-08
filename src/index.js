6e8e38ad99ad7ba70c5e792bd59134f415744363
Daoud-BackEnd <daoud.bashir.developer@gmail.com>
2023-04-08 04:26:26 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import './Resources/fonts/walsheim/GTWalsheimPro-Medium.ttf';
+import './Resources/fonts/walsheim/GTWalsheimPro-Regular.ttf';
+import './Resources/fonts/walsheim/GTWalsheimPro-Light.ttf';
+import './Resources/fonts/walsheim/GTWalsheimPro-Bold.ttf';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
